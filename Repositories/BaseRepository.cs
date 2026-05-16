fe90e923eda314b56df8d3e5b074d6ff28664f26
EzX_Anub1s <ivantop625638@gmail.com>
2026-05-16 10:35:45 +0300

    fix create tournament

--- Repositories/BaseRepository.cs
+++ Repositories/BaseRepository.cs
@@ -1,7 +1,6 @@
 ﻿using MongoDB.Driver;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
-
 namespace Beckend.Repositories
 {
     public abstract class BaseRepository<T> where T : class
@@ -11,31 +10,24 @@ namespace Beckend.Repositories
         protected BaseRepository(IConfiguration config, string collectionName)
         {
             var connectionString = config.GetConnectionString("MongoDB");
-
             if (string.IsNullOrEmpty(connectionString))
             {
                 throw new InvalidOperationException("MongoDB connection string is not configured");
             }
-
             var client = new MongoClient(connectionString);
             var database = client.GetDatabase("CreateadTournament");
             _collection = database.GetCollection<T>(collectionName);
         }
-
         public async Task<List<T>> GetAllAsync() =>
             await _collection.Find(_ => true).ToListAsync();
-
         public async Task<T?> GetByIdAsync(string id)
         {
             if (!ObjectId.TryParse(id, out var objectId))
                 return null;
 
             return await _collection.Find(Builders<T>.Filter.Eq("_id", objectId)).FirstOrDefaultAsync();
-        }
-
         public async Task CreateAsync(T entity) =>
             await _collection.InsertOneAsync(entity);
-
         public async Task UpdateAsync(string id, T entity)
         {
             if (!ObjectId.TryParse(id, out var objectId))
@@ -43,7 +35,6 @@ namespace Beckend.Repositories
 
             await _collection.ReplaceOneAsync(Builders<T>.Filter.Eq("_id", objectId), entity);
         }
-
         public async Task DeleteAsync(string id)
         {
             if (!ObjectId.TryParse(id, out var objectId))
